2675861def56a28b0b30646b1f1170b1709c7751
AMEEN ULLAH <19B-089-SE@uit.edu>
2023-01-12 17:23:08 +0500

    First

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-const app = require('express')()
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
+const app = require("express")()
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const ejs = require("ejs");
+
+const vehicle = require("./models/vehicle")
 
 dotenv.config()
+mongoose.connect(process.env.URI + "/vehicle")
+    .then(res => {
+        console.log("Connected to MongoDB server")
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
-mongoose.connect(process.env.URI)
-.then(res=>{
-    console.log("Connected to MongoDB Server")
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set("view engine", "ejs")
+
+
+
+//routes
+
+app.post("/save",async(req,res)=>{
+    const newVehicle = await vehicle.create(req.body)
+    await newVehicle.save()
+    res.send("Added!")
 })
-.catch(err=>{
-    console.log(err)
+app.get("/", async(req, res) => {
+    const vehicles = await vehicle.find();
+    res.render("index",{
+        vehicles:vehicles
+    })
 })
 
-app.use(bodyParser.urlencoded())
 
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
-
-app.listen(process.env.PORT,()=>{
+//server
+app.listen(process.env.PORT, () => {
     console.log(`Server running on http://localhost:${process.env.PORT}`)
 })
